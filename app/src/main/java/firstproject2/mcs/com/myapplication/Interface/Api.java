9342d5b5fb7d2f2772b303503b52bee636053ec4
Sherdon Brown <sherdonbrowns88@gmail.com>
2018-12-21 08:48:51 -0500

    Initial commit

---
+++ app/src/main/java/firstproject2/mcs/com/myapplication/Interface/Api.java
@@ -0,0 +1,14 @@
+package firstproject2.mcs.com.myapplication.Interface;
+
+import java.util.List;
+
+import firstproject2.mcs.com.myapplication.Hero;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface Api {
+    String BASE_URL = "https://simplifiedcoding.net/demos/";
+
+    @GET("marvel/")
+    Call<List<Hero>> getHeroes();
+}
